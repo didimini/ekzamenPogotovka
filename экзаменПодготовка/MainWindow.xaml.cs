1cb5748c8b03046c71619eb42334f0d47d4f46e8
Диана Минияхметова <dianamilk2016@outlook.com>
2024-02-08 23:24:44 +0500

    commit two

--- экзаменПодготовка/MainWindow.xaml.cs
+++ экзаменПодготовка/MainWindow.xaml.cs
@@ -21,7 +21,8 @@ namespace экзаменПодготовка
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        string tarifName;
+        double resultMain;
         public MainWindow()
         {
             InitializeComponent();
@@ -43,26 +44,32 @@ namespace экзаменПодготовка
                 {
                     if (time <= 200)
                     {
-                        resultCost.Content = $"К оплате: {0.7 * time}";
+                        resultMain = 0.7 * time;
+                        resultCost.Content = $"К оплате: {resultMain}";
                         minSvverh.Visibility = Visibility.Collapsed;
+                        tarifName = "Тариф 1";
                     }
                     else
                     {
-                        resultCost.Content = $"К оплате: {140 + 1.6 * (time - 200)}";
+                        resultMain = 140 + 1.6 * (time - 200);
+                        resultCost.Content = $"К оплате: {resultMain}";
                         minSvverh.Content = $"Минут свверх установленной нормы {time - 200}";
                         minSvverh.Visibility = Visibility.Visible;
+                        tarifName = "Тариф 2";
                     }
                 }
                 else if (tarifTwo.IsChecked == true)
                 {
                     if (time <= 100)
                     {
-                        resultCost.Content = $"К оплате: {0.3 * time}";
+                        resultMain = 0.3 * time;
+                        resultCost.Content = $"К оплате: {resultMain}";
                         minSvverh.Visibility = Visibility.Collapsed;
                     }
                     else
                     {
-                        resultCost.Content = $"К оплате: {30 + 1.6 * (time - 100)}";
+                        resultMain = 30 + 1.6 * (time - 100);
+                        resultCost.Content = $"К оплате: {resultMain}";
                         minSvverh.Content = $"Минут свверх установленной нормы {time - 100}";
                         minSvverh.Visibility = Visibility.Visible;
                     }
@@ -72,14 +79,14 @@ namespace экзаменПодготовка
 
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            // Используем регулярное выражение для проверки, что вводимые символы - это числа
+            // регулярное выражение для проверки ввода только чисел
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
         }
 
         private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            // Запрещаем вставку текста с помощью Ctrl+V
+            // запрет вставки текста с помощью Ctrl+V
             if (e.Key == Key.V && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
             {
                 e.Handled = true;
@@ -99,6 +106,40 @@ namespace экзаменПодготовка
             this.Close();
         }
 
-        
+        private void cvitocBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текущую дату и время в формате
+            string dateNow = DateTime.Now.ToString("yyyy.MM.dd_HH.mm.ss");
+
+            // Получаем текущий каталог приложения
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Формируем путь к файлу шаблона договора
+            string templatePath = System.IO.Path.Combine(currentDirectory, "Chek", "Чек.docx");
+            string outputPath = System.IO.Path.Combine(currentDirectory, "Chek", $"Чек_{dateNow}.docx ");
+
+            // Создаем объект приложения Microsoft Word
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            // Открываем шаблон договора
+            Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Open(templatePath);
+
+            // Заменяем метки в шаблоне значениями
+            doc.Content.Find.Execute(FindText: "{Tarif}", ReplaceWith: tarifName.ToString());
+            doc.Content.Find.Execute(FindText: "{Result}", ReplaceWith: resultMain.ToString());
+            doc.Content.Find.Execute(FindText: "{Date}", ReplaceWith: dateNow.ToString());
+
+            // Сохраняем документ с замененными значениями по указанному пути
+            doc.SaveAs2(outputPath);
+            // Закрываем документ
+            doc.Close();
+            // Закрываем приложение Microsoft Word
+            wordApp.Quit();
+
+            // Открываем созданный чек
+            System.Diagnostics.Process.Start(outputPath);
+
+            // Выводим сообщение об успешном выполнении операции
+            MessageBox.Show("Операция успешно выполнена. Чек сохранен.");
+        }
     }
 }
